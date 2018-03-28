f717b09838371c83947b126ed132983a6400a74e
NIMANTHA-PC\User <vsnkariyakarawana@gmail.com>
2018-03-28 14:28:05 +0530

    Moving Window to the extended screen

---
+++ WindowMovingToSecondaryScreen/WindowMovingToSecondaryScreen/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowMovingToSecondaryScreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //SecondWindow.getInstance().Show();
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+
+                Application.Current.Dispatcher.Invoke(new Action(() =>
+                {
+                    SecondWindow.getInstance().Show();
+                }));
+
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            SecondWindow.getInstance().Show();
+           // SecondWindow.getInstance().Focus();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            SecondWindow.getInstance().Close();
+            Environment.Exit(0);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SecondWindow.getInstance().Hide();
+        }
+
+        private void TextBoxMessage_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SecondWindow.getInstance().ChangeText(TextBoxMessage.Text);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SecondWindow.getInstance().MoveToSecondryScreen();
+        }
+    }
+}
---
+++ WindowMovingToSecondaryScreen/WindowMovingToSecondaryScreen/SecondWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WindowsInput;
+
+namespace WindowMovingToSecondaryScreen
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        private static SecondWindow myInstance;
+
+        private static object paddleLock = new object();
+        private SecondWindow()
+        {
+            InitializeComponent();
+
+            Loaded += SecondWindow_Loaded;
+        }
+
+        public static SecondWindow getInstance()
+        {
+            if (myInstance == null)
+            {
+                lock (paddleLock)
+                {
+                    if (myInstance == null)
+                    {
+                        myInstance = new SecondWindow();
+                    }
+                }
+            }
+            return myInstance;
+        }
+
+        void SecondWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            MoveToSecondryScreen();
+        }
+
+        public void MoveToSecondryScreen()
+        {
+            this.Focus();
+            InputSimulator insim = new InputSimulator();
+            insim.Keyboard.ModifiedKeyStroke(new[] { WindowsInput.Native.VirtualKeyCode.LWIN, WindowsInput.Native.VirtualKeyCode.SHIFT }, new[] { WindowsInput.Native.VirtualKeyCode.RIGHT });
+        }
+
+        public void ChangeText(string text)
+        {
+            TextBlockMessage.Text = text;
+        }
+    }
+}
