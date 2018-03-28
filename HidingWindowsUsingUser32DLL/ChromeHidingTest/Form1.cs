cb3dbfa35f66f1f06888ad021ed18d889a544561
NIMANTHA-PC\User <vsnkariyakarawana@gmail.com>
2018-03-28 14:32:21 +0530

    Hiding Windows inside a windows Form

---
+++ HidingWindowsUsingUser32DLL/ChromeHidingTest/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChromeHidingTest
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process p = Process.Start( @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+                p.WaitForInputIdle();
+                Thread.Sleep(5000);
+                SetParent(p.MainWindowHandle, this.Handle);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
