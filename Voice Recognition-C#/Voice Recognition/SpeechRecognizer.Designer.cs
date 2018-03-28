449a52da9037143385580b20e0ed63a1a10f6e96
NIMANTHA-PC\User <vsnkariyakarawana@gmail.com>
2018-03-28 10:50:45 +0530

    Test

---
+++ Voice Recognition-C#/Voice Recognition/SpeechRecognizer.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Voice_Recognition
+{
+    partial class SpeechRecognizer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpeechRecognizer));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnClearLog = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cmbVoices = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(435, 237);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "-Log-";
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(0, 381);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(125, 23);
+            this.btnEnable.TabIndex = 1;
+            this.btnEnable.Text = "Enable Voice Control";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Enabled = false;
+            this.btnDisable.Location = new System.Drawing.Point(310, 381);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(125, 23);
+            this.btnDisable.TabIndex = 2;
+            this.btnDisable.Text = "Disable Voice Control";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click_1);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(187, 349);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(58, 19);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Inactive";
+            // 
+            // btnClearLog
+            // 
+            this.btnClearLog.Location = new System.Drawing.Point(310, 350);
+            this.btnClearLog.Name = "btnClearLog";
+            this.btnClearLog.Size = new System.Drawing.Size(125, 22);
+            this.btnClearLog.TabIndex = 5;
+            this.btnClearLog.Text = "Clear Log";
+            this.btnClearLog.UseVisualStyleBackColor = true;
+            this.btnClearLog.Click += new System.EventHandler(this.btnClearLog_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter text to speak and press Enter";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 293);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(426, 52);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // cmbVoices
+            // 
+            this.cmbVoices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbVoices.FormattingEnabled = true;
+            this.cmbVoices.Location = new System.Drawing.Point(258, 270);
+            this.cmbVoices.Name = "cmbVoices";
+            this.cmbVoices.Size = new System.Drawing.Size(173, 21);
+            this.cmbVoices.TabIndex = 8;
+            this.cmbVoices.SelectedIndexChanged += new System.EventHandler(this.cmbVoices_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Select/Change Voice";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(5, 350);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(107, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Enable Speaking";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // SpeechRecognizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 408);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbVoices);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClearLog);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SpeechRecognizer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Speech Recognition";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnClearLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cmbVoices;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Voice Recognition-C#/Voice Recognition/SpeechRecognizer.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Voice_Recognition
+{
+    public partial class SpeechRecognizer : Form
+    {
+        SpeechRecognitionEngine RecEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer Synth = new SpeechSynthesizer();
+        bool isRecognitionActive = false;
+        bool isSpeakingEnabled = false;
+        public SpeechRecognizer()
+        {
+            InitializeComponent();
+            this.richTextBox1.Enabled = true;
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.GotFocus += new System.EventHandler(this.RTBGotFocus);
+        
+        }
+        /// <summary>
+        /// change the richtextbox to editable false
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RTBGotFocus(object sender, EventArgs e)
+        {
+            System.Windows.Forms.SendKeys.Send("{tab}");
+        }
+        
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            if (!isRecognitionActive)
+            {
+                RecEngine.RecognizeAsync(RecognizeMode.Multiple);
+                isRecognitionActive = true;
+                btnDisable.Enabled = true;
+                lblStatus.Text = "Active";
+                lblStatus.ForeColor = Color.Green;
+                btnEnable.Enabled = false;
+               
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices Commands = new Choices();
+            Commands.Add(new string[] {"hey","hello","name","hey jarvis",
+                                        "hey sampath","ok google","ok",
+                                        "okay","google","apple","orange",
+                                        "colombo","new york","sri lanka","computer",
+                                        "watch","ok sampath","hi sampath","hello sampath"});
+            //{ "ok sampath", "hey sampath", "sampath", "hello", "ok" });
+            GrammarBuilder GBuilder = new GrammarBuilder();
+            GBuilder.Append(Commands);
+            Grammar GrammarObj = new Grammar(GBuilder);
+
+            //recEngine.BabbleTimeout = TimeSpan.FromSeconds(10);
+
+            RecEngine.LoadGrammarAsync(GrammarObj);
+            RecEngine.SetInputToDefaultAudioDevice();
+            RecEngine.SpeechRecognized += recEngine_SpeechRecognized;
+
+            RecEngine.SpeechDetected += recEngine_SpeechDetected;
+            RecEngine.RecognizeCompleted += recEngine_RecognizeCompleted;
+            RecEngine.SpeechHypothesized += recEngine_SpeechHypothesized;
+            RecEngine.SpeechRecognitionRejected += recEngine_SpeechRecognitionRejected;
+
+            foreach(InstalledVoice InstalledVoice in Synth.GetInstalledVoices())
+            {
+                VoiceInfo Info = InstalledVoice.VoiceInfo;
+                cmbVoices.Items.Add(Info.Name);
+            }
+
+            VoiceInfo Vinfo = Synth.Voice;
+            cmbVoices.SelectedItem = Vinfo.Name;
+
+        }
+
+        void recEngine_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            richTextBox1.Text += "\n  SpeechRecognitionRejected " + e.Result.Text;
+            if (isSpeakingEnabled)
+            {
+                Synth.SpeakAsync("Rejected Speech is " + e.Result.Text);
+            }
+        }
+
+        void recEngine_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            richTextBox1.Text += "\n  Speech Hypothesized "+ e.Result.Text;
+            if (isSpeakingEnabled)
+            {
+                Synth.SpeakAsync("Hypothesized Speech is " + e.Result.Text);
+            }
+        }
+
+        void recEngine_SpeechDetected(object sender, SpeechDetectedEventArgs e)
+        {
+            richTextBox1.Text += "\n  " + DateTime.Now.ToString("yyyy-MM-dd h:mm:ss tt") +" Speech Detected - ";
+        }
+
+        void recEngine_RecognizeCompleted(object sender, RecognizeCompletedEventArgs e)
+        {
+            richTextBox1.Text += "\n Completed ";
+        }
+
+        void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //e.Result.Confidence.Equals(1.0);
+            if (e.Result.Confidence > 0.1)
+            {
+                richTextBox1.Text += " Confidence level = " + e.Result.Confidence.ToString();
+
+                switch (e.Result.Text)
+                {
+                    /* case "hello":
+                        // MessageBox.Show("Hello Triggered");
+                         richTextBox1.Text += " hello";
+                         break;
+                     case "name":
+                        // MessageBox.Show("name");
+                         richTextBox1.Text += " name";
+                         break;
+                     case "hey jarvis":
+                         richTextBox1.Text += " hey jarvis";
+                         break;
+                     case "hey sampath":
+                         richTextBox1.Text += " hey sampath";
+                         break;
+                     case "ok google":
+                         richTextBox1.Text += " ok google";
+                         break;
+                     case "hey":
+                         richTextBox1.Text += " hey";
+                         break;*/
+                    default:
+                        richTextBox1.Text += " ### Detected Speech = " + e.Result.Text;
+                        if (isSpeakingEnabled)
+                        {
+                            Synth.SpeakAsync("Detected Speech is " + e.Result.Text);
+                        }
+                        break;
+                }
+
+                foreach (RecognizedPhrase Phrase in e.Result.Alternates)
+                {
+                    richTextBox1.Text += " ### Alternates = "+ Phrase.Text;
+                }
+            }
+        }
+
+        private void btnDisable_Click_1(object sender, EventArgs e)
+        {
+            RecEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+            isRecognitionActive = false;
+            lblStatus.Text = "Inactive";
+            lblStatus.ForeColor = Color.Red;
+            btnEnable.Enabled = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            RecEngine.RecognizeAsyncStop();
+        }
+
+        private void btnClearLog_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "-Log-";
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (isSpeakingEnabled)
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    if (textBox1.Text != "")
+                    {
+                        Synth.SpeakAsync(textBox1.Text);
+                        textBox1.Text = "";
+                    }
+                    else
+                    {
+                        Synth.SpeakAsync("Please enter something to talk");
+                    }
+                }
+            }
+        }
+
+        private void cmbVoices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbVoices.SelectedIndex != -1)
+            {
+                try
+                {
+                    Synth.SelectVoice(cmbVoices.SelectedItem.ToString());
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                isSpeakingEnabled = true;
+            }
+            else
+            {
+                isSpeakingEnabled = false;
+            }
+        }
+    }
+}
