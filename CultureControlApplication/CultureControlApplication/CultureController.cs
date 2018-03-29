15a5572c5201a3cb036e403f98cc44f5aa98ffd1
NIMANTHA-PC\User <vsnkariyakarawana@gmail.com>
2018-03-29 10:21:03 +0530

    Culture Controller Application

---
+++ CultureControlApplication/CultureControlApplication/CultureController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CultureControlApplication
+{
+    class CultureController
+    {
+        public class CulctureController
+        {
+            private static CulctureController _myInstance;
+            private static object _padLock = new object();
+
+            private List<ILanguageChangedListener> _subscribedLanguageChangedListenersList;
+
+
+            private CulctureController()
+            {
+                _subscribedLanguageChangedListenersList = new List<ILanguageChangedListener>();
+            }
+
+
+            public static CulctureController GetInstance()
+            {
+                if (_myInstance == null)
+                {
+                    lock (_padLock)
+                    {
+                        if (_myInstance == null)
+                        {
+                            _myInstance = new CulctureController();
+                        }
+                    }
+                }
+                return _myInstance;
+            }
+
+            public void ChangeCultureLanguage(Langauge langauge)
+            {
+                switch (langauge)
+                {
+                    case Langauge.English:
+                        System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+                        System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+                        break;
+
+                    case Langauge.Sinhala:
+                        System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("si-LK");
+                        System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("si-LK");
+                        break;
+
+                    case Langauge.Tamil:
+                        System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ta-IN");
+                        System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ta-IN");
+                        break;
+
+                    case Langauge.Arabic:
+                        System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ar-AE");
+                        System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ar-AE");
+                        break;
+                }
+
+                this.NotifySubscribers();
+            }
+
+
+
+
+            public void RegisterUserInterface(ILanguageChangedListener iLanguageChangedListener)
+            {
+
+                //If the passsed object is same type, remove the old one
+                foreach (var item in this._subscribedLanguageChangedListenersList)
+                {
+                    if (item.GetType() == iLanguageChangedListener.GetType())
+                    {
+                        this.RemoveRegisteredUserInterface(iLanguageChangedListener);
+                        break;
+                    }
+                }
+
+                _subscribedLanguageChangedListenersList.Add(iLanguageChangedListener);
+
+            }
+
+            public void RemoveRegisteredUserInterface(ILanguageChangedListener iLanguageChangedListener)
+            {
+                int i = 0;
+                foreach (var item in this._subscribedLanguageChangedListenersList)
+                {
+                    if (item.GetType() == iLanguageChangedListener.GetType())
+                    {
+                        _subscribedLanguageChangedListenersList.RemoveAt(i);
+                        break;
+                    }
+                    i++;
+                }
+
+                //if (_subscribedLanguageChangedListenersList.Contains(iLanguageChangedListener))
+                //{
+                //    _subscribedLanguageChangedListenersList.Remove(iLanguageChangedListener);
+                //}
+            }
+
+            private void NotifySubscribers()
+            {
+                foreach (var item in this._subscribedLanguageChangedListenersList)
+                {
+                    item.OnLanguageChanged();
+                }
+            }
+
+
+        }
+
+        public enum Langauge
+        {
+            English,
+            Sinhala,
+            Tamil,
+            Arabic
+        }
+
+        public interface ILanguageChangedListener
+        {
+            void OnLanguageChanged();
+        }
+    }
+}
---
+++ CultureControlApplication/CultureControlApplication/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CultureControlApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,CultureController.ILanguageChangedListener
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+            ComboBoxCultureList.SelectionChanged += ComboBoxCultureList_SelectionChanged;
+
+           
+
+            ComboBoxCultureList.Items.Clear();
+            ComboBoxCultureList.Items.Add("Sinhala");
+            ComboBoxCultureList.Items.Add("English");
+            ComboBoxCultureList.Items.Add("Tamil");
+
+            CultureController.CulctureController.GetInstance().RegisterUserInterface(this);
+        }
+
+
+
+        private void ComboBoxCultureList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            switch (ComboBoxCultureList.SelectedItem.ToString())
+            {
+                case "English":
+                    CultureController.CulctureController.GetInstance().ChangeCultureLanguage(CultureController.Langauge.English);
+                    break;
+                case "Tamil":
+                    CultureController.CulctureController.GetInstance().ChangeCultureLanguage(CultureController.Langauge.Tamil);
+                    break;
+                case "Sinhala":
+                    CultureController.CulctureController.GetInstance().ChangeCultureLanguage(CultureController.Langauge.Sinhala);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LabelDemo.Content = Properties.Resources.LABELTEXT;
+        }
+
+        public void OnLanguageChanged()
+        {
+            LabelDemo.Content = Properties.Resources.LABELTEXT;
+        }
+    }
+}
