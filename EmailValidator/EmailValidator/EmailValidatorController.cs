0bb10a36766e9206514d80254a085fe42b3e3a24
NIMANTHA-PC\User <vsnkariyakarawana@gmail.com>
2018-04-06 15:13:06 +0530

    Email Validator Sample

---
+++ EmailValidator/EmailValidator/EmailValidatorController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailValidator
+{
+    class EmailValidatorController
+    {
+        public static bool IsEmailValid(string email)
+        {
+            string expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+
+            if (Regex.IsMatch(email, expresion))
+            {
+                if (Regex.Replace(email, expresion, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ EmailValidator/EmailValidator/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmailValidator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ButtonCheck.Click += ButtonCheck_Click;
+        }
+
+        private void ButtonCheck_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxInput.Text != null)
+            {
+                if (EmailValidator.EmailValidatorController.IsEmailValid(TextBoxInput.Text))
+                {
+                    MessageBox.Show("Valid Email!!", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("invalid Email!!", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Text Box Cannot be empty!!", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
